2b70fe3f5839b442a35b0fe7434d5fee4ee26f93
ehsan2793 <ehsan2793@gmail.com>
2024-08-15 20:48:54 -0700

    first commit

---
+++ budget/Models/Extentions/BudgetCategory+CoreDataClass.swift
@@ -0,0 +1,14 @@
+//
+//  BudgetCategory+CoreDataClass.swift
+//  budget
+//
+//  Created by Ehsan Rahimi on 8/15/24.
+//
+
+import Foundation
+import CoreData
+
+@objc(BudgetCategory)
+public class BudgetCategory: NSManagedObject {
+    
+}
